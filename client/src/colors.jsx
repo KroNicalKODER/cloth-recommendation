768f2e3bc850883c625de54171b3efe01ff0a4ca
Yash Veer <135231980+webmaestro2002@users.noreply.github.com>
2024-08-12 16:49:10 +0530

    Create colors.jsx

---
+++ client/src/colors.jsx
@@ -0,0 +1,7 @@
+const colors = {
+  "Dark_deep": ["red", "black", "gold", "green"],
+  "Fair_light": ["pink", "lightBlue", "peach", "wheat"],
+  "Medium_tone": ["gold", "red", "olive", "blue"]
+};
+
+export default colors;
